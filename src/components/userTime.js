8bef4b75eed15ff56117eb57eafdfcbc065fda4b
Chris Hill <chrishillbuilds@gmail.com>
2019-12-05 16:30:02 -0500

    cleaned up code and added updateHours function

--- src/components/userTime.js
+++ src/components/userTime.js
@@ -1,51 +1,104 @@
-import React, { useState } from "react";
-import { loadData, updateData } from "../utils/loadData";
+import React, { useState, useEffect } from "react";
+import { loadData, updateData, newData } from "../utils/loadData";
 import moment from "moment";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 import CardDeck from "react-bootstrap/CardDeck";
 import Button from "react-bootstrap/Button";
 
-var questionList = [];
-var timeData;
+
 
 const TimeParams = () => {
   const [time, setTime] = useState([]);
-  const [worked, setWorked] = useState({});
+  const [lastPunch, setLastPunch] = useState('');
+  const [isClockedIn, setIsClockedIn] = useState('');
+  const [worked, setWorked] = useState('');
+  const [totalWorked, setTotalWorked] = useState('');
+  const [dayHoursArray, setDayHoursArray] = useState('');
 
-  const handleSubmit = async e => {
-    e.preventDefault();
-    console.log("time: ", time);
+
+  useEffect(() => {
+    refreshData();
+  }, [])
+
+
+  // setTimeout(() => {
+  //   setCurrentTime(moment());
+  //   let hours = currentTime.diff(lastPunch, 'minutes')
+  //   setWorked((hours / 60).toFixed(2));
+  //   //console.log('worked is:', worked);
+  //   //setWorked(moment())
+  // }, 1000);
+
+  const refreshData = async () => {
     const url = "http://localhost:3001/time/1";
-    timeData = await loadData(url);
+    const timeData = await loadData(url);
+    let punchIn = (timeData[timeData.length - 1].endtime == null) ? moment(timeData[timeData.length - 1].starttime) : moment();
+    let calcMins = moment().diff(punchIn, "seconds");
+    let calcHours = (calcMins / 3600).toFixed(2);
+    setLastPunch(punchIn);
+    let hoursArray = []
+    timeData.map(punch => {
+      hoursArray.push(parseFloat(punch.hours == null ? 0.00 : punch.hours));
+      return;
+    }
+    );
+    hoursArray.push(parseFloat(calcHours));
+    setDayHoursArray(hoursArray);
 
-    // console.log("this is the punchIn:", punchIn);
-    // console.log("this is the currentTime:", currentTime);
-    // console.log("this is the answer:", calcHours);
+    const add = (a, b) =>
+      a + b
+    // use reduce to sum our array
+    const sum = hoursArray.reduce(add);
+
+    setIsClockedIn((timeData[timeData.length - 1].endtime == null) ? true : false);
+    setWorked(calcHours);
     setTime(timeData);
-  };
+    setTotalWorked(sum.toFixed(2));
+  }
+
+  const updateHours = () => {
+    let calcMins = moment().diff(lastPunch, "seconds");
+    let calcHours = (calcMins / 3600).toFixed(2);
+    let hoursArray = dayHoursArray;
+
+    hoursArray.pop()
+    hoursArray.push(parseFloat(calcHours));
+
+    const add = (a, b) =>
+      a + b
+    // use reduce to sum our array
+    const sum = hoursArray.reduce(add);
+
+    setWorked(calcHours);
+    setTotalWorked(sum.toFixed(2));
+  }
 
   const clockOut = async e => {
     e.preventDefault();
-    console.log("time: ", time);
     const url = "http://localhost:3001/time/clockout/1";
-    timeData = await updateData(url);
-    handleSubmit();
-  };
+    const clockOutData = await updateData(url);
+    setLastPunch([])
+    refreshData();
+  }
 
+  const clockIn = async e => {
+    e.preventDefault();
+    const url = "http://localhost:3001/time/clockin/1";
+    const clockInData = await newData(url);
+    let punchIn = clockInData;
+
+    setLastPunch(punchIn);
+    refreshData();
+  }
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="timesheet">Get Timesheet</label>
-        <button type="submit">Submit</button>
-      </form>
+      {isClockedIn === true ? <Button variant="primary" onClick={clockOut}>Clock-Out</Button> : <Button variant="primary" onClick={clockIn}>Clock-In</Button>}
+
       <CardDeck>
         {time.map(punch => {
-          var currentTime = moment();
-          var punchIn = moment(timeData[timeData.length - 1].starttime);
-          var calcMins = currentTime.diff(punchIn, "minutes");
-          var calcHours = (calcMins / 60).toFixed(2);
+
 
           return (
             <>
@@ -56,37 +109,34 @@ const TimeParams = () => {
                 border="dark"
                 style={{ width: "10rem" }}
               >
-                <Card.Header key={punch.id} as="h5">
+                <Card.Header key={`date-${punch.id}`} as="h5">
                   {moment(punch.starttime).format("MM/DD/YYYY")}
                 </Card.Header>
-                <Card.Body key={punch.id}>
-                  <Card.Title key={punch.id}>Clock-In</Card.Title>
-                  <Card.Text key={punch.id}>
-                    <input
-                      type="text"
-                      style={{ width: "8rem", textAlign: "center" }}
-                      value={moment(punch.starttime).format("h:mm A")}
-                    />
+                <Card.Body key={`clockin-${punch.id}`}>
+                  <Card.Title>Clock-In</Card.Title>
+                  <Card.Text key={`clockindetail-${punch.id}`}>
+                    {moment(punch.starttime).format("h:mm A")}
+
                   </Card.Text>
-                  <Card.Title key={punch.id}>
+                  <Card.Title key={`clockout-${punch.id}`}>
                     {" "}
                     {punch.endtime == null ? "" : "Clock-Out"}
                   </Card.Title>
-                  <Card.Text key={punch.id}>
+                  <Card.Text key={`clockoutdetail-${punch.id}`}>
                     {punch.endtime == null
                       ? ""
                       : moment(punch.endtime).format("h:mm A")}
                   </Card.Text>
                 </Card.Body>
-                <Card.Footer key={punch.id}>
-                  <Card.Title>Hours</Card.Title>
-                  <Card.Text>
-                    {punch.endtime == null ? calcHours : punch.hours}
+                <Card.Footer key={`Hoursfooter-${punch.id}`}>
+                  <Card.Title key={`hours-${punch.id}`}>Hours</Card.Title>
+                  <Card.Text key={`hoursdetail-${punch.id}`}>
+                    {punch.endtime == null ? worked : punch.hours}
                   </Card.Text>
                 </Card.Footer>
                 {punch.endtime == null ? (
-                  <Card.Footer key={punch.id}>
-                    <Button variant="primary" onClick={clockOut}>Clock-Out</Button>
+                  <Card.Footer key={`buttonfooter-${punch.id}`}>
+                    <Button key={`button-${punch.id}`} variant="primary" onClick={updateHours}>Refresh Hours</Button>
                   </Card.Footer>
                 ) : (
                     ""
@@ -96,6 +146,15 @@ const TimeParams = () => {
           );
         })}
       </CardDeck>
+      <Card key='totalhours'>
+        <Card.Header as="h5">
+          Total Hours for Period
+                </Card.Header>
+        <Card.Body>
+          <Card.Title>{totalWorked}</Card.Title>
+          {isClockedIn === true ? <Button variant="primary" onClick={updateHours}>Refresh Hours</Button> : <Button variant="primary" onClick={updateHours} hidden>Refresh Hours</Button>}
+        </Card.Body>
+      </Card>
     </>
   );
 };
--- src/utils/loadData.js
+++ src/utils/loadData.js
@@ -8,6 +8,16 @@ export const loadData = async url => {
 
 export const updateData = async url => {
   const response = await fetch(url, { method: 'PUT' });
+  // console.log(await response);
+  // const data = await response.json();
+  // return data;
+  return;
+};
+
+export const newData = async url => {
+  const response = await fetch(url, { method: 'POST' });
   const data = await response.json();
-  return data;
+  // return data;
+  console.log(data.rows[0].starttime);
+  return data.rows[0].starttime;
 };
